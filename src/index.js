e2c3b24ebe2bcfb26530f3cbeabef7541a988896
Brett Reinhard <brett@reinhards.us>
2017-09-10 20:12:31 -0700

    Removed service worker

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
